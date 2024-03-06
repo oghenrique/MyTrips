ce33099878d64c1785b8788eaaf455826a1f9466
oghenrique <santosguhenrique@gmail.com>
2024-03-06 09:26:58 -0300

    faltando imgs tela 1

--- app/src/main/java/br/senai/sp/jandira/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/MainActivity.kt
@@ -7,12 +7,19 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,8 +27,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.senai.sp.jandira.ui.theme.MyTripsTheme
 
 class MainActivity : ComponentActivity() {
@@ -62,12 +71,118 @@ fun Greeting() {
                     .height(50.dp)
                     .width(150.dp)
                     .background(
-                        color = Color.Blue,
+                        color = Color(0xFFCF06F0),
                         shape = RoundedCornerShape(
                             bottomStart = 16.dp
                         )
                     ))
             }
+
+            Column(
+                modifier = Modifier
+                    .padding(20.dp)
+            ){
+                Text(
+                    text = "Login",
+                    fontSize = 60.sp,
+                    color = Color(0xFFCF06F0),
+                    fontWeight = FontWeight.ExtraBold,
+                )
+                Text(
+                    text = "Please sign in to continue.",
+                    fontSize = 16.sp,
+                    color = Color.Gray,
+                )
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = {},
+                    label = {
+                        Text(text = "Email",
+                            color = Color.Black
+                        )
+                    },
+                    colors = OutlinedTextFieldDefaults.colors(
+                        unfocusedBorderColor = Color(0xFFCF06F0),
+                        focusedBorderColor = Color(0xFFCF06F0)
+                    ),
+                    shape = RoundedCornerShape(
+                        16.dp
+                    ),
+                    modifier = Modifier
+                        .padding(
+                            top = 80.dp
+                        )
+                        .fillMaxWidth()
+                )
+
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = {},
+                    label = {
+                        Text(text = "Password",
+                            color = Color.Black
+                        )
+                    },
+                    colors = OutlinedTextFieldDefaults.colors(
+                        unfocusedBorderColor = Color(0xFFCF06F0),
+                        focusedBorderColor = Color(0xFFCF06F0)
+                    ),
+                    shape = RoundedCornerShape(
+                        16.dp
+                    ),
+                    modifier = Modifier
+                        .padding(
+                            top = 20.dp
+                        )
+                        .fillMaxWidth()
+                )
+            }
+
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth()
+                    .offset(y = -70.dp),
+                horizontalAlignment = Alignment.End
+            ) {
+                Button (
+                    onClick = { /*TODO*/ },
+                    colors = ButtonDefaults
+                        .buttonColors(
+                            containerColor = Color(0xFFCF06F0)
+                        ),
+                    shape = RoundedCornerShape(
+                        12.dp
+                    )
+
+                ){
+                    Text(
+                        text = "SIGN IN",
+                        fontSize = 18.sp,
+                        color = Color.White
+                    )
+                }
+                Row(
+                    modifier = Modifier
+                        .padding(top = 20.dp)
+                ) {
+                    Text(
+                        text = "Don’t have an account? ",
+                        fontSize = 13.sp,
+                        color = Color.Gray,
+                    )
+                    Text(
+                        text = "Sign up",
+                        fontSize = 13.sp,
+                        color = Color(0xFFCF06F0),
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                }
+
+
+            }
+
+
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
@@ -76,11 +191,11 @@ fun Greeting() {
                         .height(50.dp)
                         .width(150.dp)
                         .background(
-                            color = Color.Blue,
+                            color = Color(0xFFCF06F0),
                             shape = RoundedCornerShape(
                                 topEnd = 16.dp
                             )
-                ))
+                        ))
             }
         }
     }
