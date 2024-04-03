ef31c83cf92c0b488b36edde7705700cfb28c779
Gustavo Henrique <123705623+oghenrique@users.noreply.github.com>
2024-04-03 20:41:59 -0300

    Tela Home Finalizada

--- app/src/main/java/br/senai/sp/jandira/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/MainActivity.kt
@@ -11,35 +11,54 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -55,7 +74,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    TelaSp()
+                    Greeting()
                 }
             }
         }
@@ -64,6 +83,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting() {
+
+    var emailState = remember {
+        mutableStateOf("")
+    }
+
+    var senhaState = remember {
+        mutableStateOf("")
+    }
+
     Surface(
         modifier = Modifier
             .fillMaxSize(),
@@ -108,8 +136,13 @@ fun Greeting() {
                     color = Color.Gray,
                 )
                 OutlinedTextField(
-                    value = "",
-                    onValueChange = {},
+                    value = emailState.value,
+                    onValueChange = {
+                        emailState.value = it
+                    },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Email
+                    ),
                     label = {
                         Row {
                             Image(
@@ -140,8 +173,14 @@ fun Greeting() {
                 )
 
                 OutlinedTextField(
-                    value = "",
-                    onValueChange = {},
+                    value = senhaState.value,
+                    onValueChange = {
+                        senhaState.value = it
+                    },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Password
+                    ),
+                    visualTransformation = PasswordVisualTransformation(),
                     label = {
                         Row {
                             Image(
@@ -179,7 +218,7 @@ fun Greeting() {
                 horizontalAlignment = Alignment.End
             ) {
                 Button(
-                    onClick = { /*TODO*/ },
+                    onClick = { },
                     colors = ButtonDefaults
                         .buttonColors(
                             containerColor = Color(0xFFCF06F0)
@@ -221,7 +260,6 @@ fun Greeting() {
 
             }
 
-
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
@@ -243,7 +281,28 @@ fun Greeting() {
 
 
 @Composable
-fun TelaSp() {
+fun Tela2() {
+
+    var userState = remember {
+        mutableStateOf("")
+    }
+
+    var phoneState = remember {
+        mutableStateOf("")
+    }
+
+    var emailState = remember {
+        mutableStateOf("")
+    }
+
+    var senhaState = remember {
+        mutableStateOf("")
+    }
+
+    var opcaoState = remember {
+        mutableStateOf(true)
+    }
+
     Surface(
         modifier = Modifier
             .fillMaxSize(),
@@ -307,7 +366,7 @@ fun TelaSp() {
                         modifier = Modifier
                             .fillMaxSize(),
                         painter = painterResource(id = R.drawable.photo),
-                        contentDescription = "Imagem de um homem sorrindo",
+                        contentDescription = "Imagem de perfil",
                         contentScale = ContentScale.Crop
                     )
 
@@ -315,10 +374,11 @@ fun TelaSp() {
 
                 Image(
                     painterResource(
-                        id = R.drawable.camera),
+                        id = R.drawable.camera
+                    ),
                     contentDescription = "",
                     modifier = Modifier
-                        .offset(y = -20.dp, x = 35.dp )
+                        .offset(y = -20.dp, x = 35.dp)
                 )
 
             }
@@ -328,8 +388,10 @@ fun TelaSp() {
                     .offset(y = -60.dp)
             ) {
                 OutlinedTextField(
-                    value = "",
-                    onValueChange = {},
+                    value = userState.value,
+                    onValueChange = {
+                        userState.value = it
+                    },
                     label = {
                         Row {
                             Image(
@@ -358,8 +420,13 @@ fun TelaSp() {
                         .fillMaxWidth()
                 )
                 OutlinedTextField(
-                    value = "",
-                    onValueChange = {},
+                    value = phoneState.value,
+                    onValueChange = {
+                        phoneState.value = it
+                    },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Number
+                    ),
                     label = {
                         Row {
                             Image(
@@ -388,8 +455,13 @@ fun TelaSp() {
                         .fillMaxWidth()
                 )
                 OutlinedTextField(
-                    value = "",
-                    onValueChange = {},
+                    value = emailState.value,
+                    onValueChange = {
+                        emailState.value = it
+                    },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Email
+                    ),
                     label = {
                         Row {
                             Image(
@@ -420,8 +492,14 @@ fun TelaSp() {
                 )
 
                 OutlinedTextField(
-                    value = "",
-                    onValueChange = {},
+                    value = senhaState.value,
+                    onValueChange = {
+                        senhaState.value = it
+                    },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Password
+                    ),
+                    visualTransformation = PasswordVisualTransformation(),
                     label = {
                         Row {
                             Image(
@@ -454,8 +532,10 @@ fun TelaSp() {
                     verticalAlignment = Alignment.CenterVertically
                 ) {
                     Checkbox(
-                        checked = false,
-                        onCheckedChange = {},
+                        checked = opcaoState.value,
+                        onCheckedChange = {
+                            opcaoState.value = it
+                        },
                         colors = CheckboxDefaults.colors(
                             checkedColor = Color(0xFFCF06F0),
                             uncheckedColor = Color(0xFFCF06F0)
@@ -543,6 +623,255 @@ fun TelaSp() {
     }
 }
 
+@Composable
+fun Tela3() {
+
+    var searchState = remember {
+        mutableStateOf("")
+    }
+
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = Color(0xffF6F6F6)
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+            ) {
+                Image(
+                    painterResource(id = R.drawable.paris),
+                    contentDescription = "Paris",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(200.dp)
+                )
+                Column(
+                    horizontalAlignment = Alignment.End
+                ) {
+                    Card(
+                        modifier = Modifier
+                            .padding(10.dp)
+                            .size(60.dp),
+                        shape = CircleShape,
+                        border = BorderStroke(2.dp, Color.White)
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.photo),
+                            contentDescription = "Foto de Perfil",
+                            contentScale = ContentScale.Crop,
+                        )
+                    }
+
+                    Row(
+                        horizontalArrangement = Arrangement.End,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(end = 10.dp),
+                    ) {
+                        Text(
+                            text = "Gustavo Henrique",
+                            color = Color.White
+                        )
+                    }
+                }
+                Column(
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .padding(start = 20.dp, bottom = 10.dp),
+                    verticalArrangement = Arrangement.Bottom
+                ) {
+                    Row(
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.LocationOn,
+                            contentDescription = "Localização",
+                            tint = Color.White,
+                            modifier = Modifier
+                                .size(20.dp)
+                        )
+                        Text(
+                            text = "You're in Paris",
+                            color = Color.White
+                        )
+                    }
+
+                    Row(
+                        modifier = Modifier.padding(start = 6.dp)
+                    ) {
+                        Text(
+                            text = "My Trips",
+                            fontWeight = FontWeight.Bold,
+                            color = Color.White,
+                            fontSize = 24.sp
+                        )
+                    }
+                }
+            }
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(150.dp)
+                    .padding(top = 20.dp, start = 20.dp)
+            ) {
+                Row {
+                    Text(text = "Categories")
+                }
+
+                Spacer(modifier = Modifier.height(10.dp))
+                LazyRow(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    items(5) {
+                        Button(
+                            onClick = {},
+                            shape = RoundedCornerShape(10.dp),
+                            colors = ButtonDefaults
+                                .buttonColors(
+                                    containerColor = Color(0xFFCF06F0)
+                                ),
+                            modifier = Modifier
+                                .height(80.dp)
+                                .width(120.dp),
+                        ) {
+                            Column(
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ) {
+                                Row {
+                                    Image(
+                                        painterResource(id = R.drawable.montanha) ,
+                                        contentDescription = "Montanhas",
+                                        modifier = Modifier
+                                            .size(30.dp)
+                                    )
+                                }
+
+                                Row {
+                                    Text(text = "Montain")
+                                }
+                            }
+                        }
+                        Spacer(modifier = Modifier.width(10.dp))
+                    }
+                }
+            }
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+            ) {
+                OutlinedTextField(
+                    value = searchState.value,
+                    onValueChange = {
+                        searchState.value = it
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(
+                            color = Color.White,
+                            shape = RoundedCornerShape(24.dp)
+                        ),
+                    trailingIcon = {
+                        IconButton(onClick = {
+
+                        }) {
+                            Icon(
+                                imageVector = Icons.Default.Search,
+                                contentDescription = "Search your destiny",
+                                tint = Color(0xffA09C9C)
+                            )
+                        }
+                    },
+                    placeholder = {
+                        Text(
+                            text = "Search your destiny",
+                            color = Color(0xffA09C9C)
+                        )
+                    },
+                    colors = OutlinedTextFieldDefaults.colors(
+                        unfocusedBorderColor = Color.White,
+                        focusedBorderColor = Color(0xF6DDD5D5)
+                    ),
+                    shape = RoundedCornerShape(24.dp)
+                )
+            }
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(top = 20.dp, start = 20.dp, end = 20.dp)
+            ) {
+                Row {
+                    Text(text = "Past Trips")
+                }
+
+                Spacer(modifier = Modifier.height(10.dp))
+                LazyColumn(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    items(3) {
+                        Card(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(250.dp)
+                                .shadow(10.dp),
+                            colors = CardDefaults
+                                .cardColors(
+                                    containerColor = Color.White
+                                )
+                        ) {
+                            Image(
+                                painterResource(id = R.drawable.londres),
+                                contentDescription = "Paisagem de londres",
+                                contentScale = ContentScale.Crop,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(10.dp),
+                            )
+
+                            Column(
+                                modifier = Modifier
+                                    .padding(start = 10.dp)
+                            ) {
+                                Row(
+                                    modifier = Modifier
+                                        .padding( bottom = 10.dp)
+                                ) {
+                                    Text(text = "London, 2019",
+                                        color = Color(0xFFCF06F0))
+                                }
+                                Row(
+                                    modifier = Modifier
+                                        .padding( bottom = 10.dp)
+                                ) {
+                                    Text(text = "London is the capital and largest city of  the United Kingdom, with a population of just under 9 million.",
+                                        fontSize = 14.sp,
+                                        color = Color(0xffA09C9C)
+                                    )
+                                }
+                                Row(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(end = 10.dp),
+                                    horizontalArrangement = Arrangement.End
+                                ) {
+                                    Text(text = "18 Feb - 21 Feb",
+                                        color = Color(0xFFCF06F0),
+                                        fontSize = 12.sp
+                                    )
+                                }
+                            }
+                        }
+                        Spacer(modifier = Modifier.height(15.dp))
+                    }
+                }
+            }
+        }
+    }
+}
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
@@ -556,6 +885,14 @@ fun GreetingPreview() {
 @Composable
 fun Tela2Preview() {
     MyTripsTheme {
-        TelaSp()
+        Tela2()
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun Tela3Preview() {
+    MyTripsTheme {
+        Tela3()
     }
 }
